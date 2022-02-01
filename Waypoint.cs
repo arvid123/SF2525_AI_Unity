a1deb6b7f61712d181a9155b82bc5cfaa94f4d95
Arvid  Lagerqvist <arvid.lagerqvist@solidify.dev>
2022-02-01 09:58:03 +0100

    initial commit

---
+++ Waypoint.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Assets.Scrips
+{
+    class Waypoint
+    {
+        public Vector3 pos { get; set; }
+        public List<Waypoint> neighbors { get; set; }
+        public Waypoint cameFrom { get; set; }
+        public double gScore { get; set; }
+
+        public Waypoint(Vector3 position) 
+        {
+            pos = position;
+            neighbors = new List<Waypoint>();
+            gScore = double.PositiveInfinity;
+        }
+    }
+}
